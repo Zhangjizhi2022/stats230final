2489b7d0552e832d7b3f977c12ad90ce75e97490
David Mwakima <mwakima.david@gmail.com>
2023-03-19 16:23:36 -0700

    added some code.

--- coding example.R
+++ coding example.R
@@ -4,10 +4,13 @@ sigma<-matrix(c(1,0,0,1),nrow = 2,ncol = 2)
 sim<-mvrnorm(500,mean,sigma)
 y=rbinom(500,1,exp(-1+sim[,1])/(1+exp(-1+sim[,1])))
 N = length(y)
+d = 3
 n <- 100
 epsilon <- 0.005
 iter <- 100
 U <- matrix(NA, nrow=N, ncol=iter+1)
+theta <- matrix(NA, nrow=d, ncol=iter+1)
+theta[, 1] <- c(rep(0.5, d))
 U[, 1] <- c(rep(0, 400), rep(1, 100))
 for (i in 1:iter){
   set.seed(i)
@@ -23,3 +26,20 @@ for (i in 1:iter){
     U[,i+1][U.prop] <- 1
   }
   else U[,i+1] <- U[,i]
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
